ca9124fb5b4393da88d2283e175ed8e13b777e7b
LiBaiIsBusy <zhangshuji1992@sina.com>
2016-02-18 16:52:01 +0800

    express+mongodb搭建个人博客

--- app.js
+++ app.js
@@ -1,69 +1,61 @@
-var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+
+var express = require('express');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var flash = require('connect-flash');
+var multer  = require('multer');
 
 var routes = require('./routes/index');
-var reg = require('./routes/reg');
-var login = require('./routes/login');
-var logout = require('./routes/logout');
-var upload = require('./routes/upload');
+var settings = require('./settings');
+
+var fs = require('fs');
+var accessLog = fs.createWriteStream('access.log', {flags: 'a'});
+var errorLog = fs.createWriteStream('error.log', {flags: 'a'});
 
 var app = express();
 
-// view engine setup
+app.set('port', process.env.PORT || 8000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//使用靠就这个中间件 放在最前面
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true }))
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(logger({stream: accessLog}));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({
+  dest: './public/images',
+  rename: function (fieldname, filename) {
+    return filename;
+  }
+}));
 app.use(cookieParser());
+app.use(session({
+  secret: settings.cookieSecret,
+  key: settings.db,//cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+  store: new MongoStore({
+    db: settings.db,
+    host: settings.host,
+    port: settings.port,
+    url:'mongodb://localhost/'+settings.db,
+    autoRemove:'native'
+  })
+}));
+app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+routes(app);
 
-app.use('/reg', reg);
-app.use('/login', login);
-app.use('/logout', logout);
-app.use('/upload', upload);
-
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (err, req, res, next) {
+  var meta = '[' + new Date() + '] ' + req.url + '\n';
+  errorLog.write(meta + err.stack + '\n');
+  next();
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-//app.listen(3000);
-module.exports = app;
--- models/user.js
+++ models/user.js
@@ -1,93 +1,74 @@
-/**
- * Created by zhangshuji on 2016/1/30.
- */
-var mysql = require('mysql');
-var DB_NAME = 'scott';
+var mongodb = require('./db');
+var crypto = require('crypto');
 
-var pool  = mysql.createPool({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'tiger'
-});
-
-pool.on('connection', function(connection) {
-    connection.query('SET SESSION auto_increment_increment=1');
-});
-
-function User(user){
-    this.account = user.account;
-    this.password = user.password;
+function User(user) {
+  this.name = user.name;
+  this.password = user.password;
+  this.email = user.email;
 };
+
 module.exports = User;
 
-pool.getConnection(function(err, connection) {
-
-    var useDbSql = "USE " + DB_NAME;
-    connection.query(useDbSql, function (err) {
+//存储用户信息
+User.prototype.save = function(callback) {
+  var md5 = crypto.createHash('md5'),
+      email_MD5 = md5.update(this.email.toLowerCase()).digest('hex'),
+      head = "http://www.gravatar.com/avatar/" + email_MD5 + "?s=48";
+  //要存入数据库的用户信息文档
+  var user = {
+      name: this.name,
+      password: this.password,
+      email: this.email,
+      head: head
+  };
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);//错误，返回 err 信息
+    }
+    //读取 users 集合
+    db.collection('users', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);//错误，返回 err 信息
+      }
+      //将用户数据插入 users 集合
+      collection.insert(user, {
+        safe: true
+      }, function (err, user) {
+        mongodb.close();
         if (err) {
-            console.log("USE Error: " + err.message);
-            return;
+          return callback(err);
         }
-        console.log('USE succeed');
+        callback(null, user[0]);//成功！err 为 null，并返回存储后的用户文档
+      });
     });
+  });
+};
 
-    //保存数据
-    User.prototype.save = function save(callback) {
-        var user = {
-            account: this.account,
-            password: this.password
-        };
-
-        var insertUser_Sql = "INSERT INTO h_user(account,password,register_time) VALUES(?,?,now())";
-
-        connection.query(insertUser_Sql, [user.account, user.password], function (err,result) {
-            if (err) {
-                console.log("insertUser_Sql Error: " + err.message);
-                return;
-            }
-
-            //connection.release();
-
-            console.log("invoked[save]");
-            callback(err,result);
-        });
-    };
-
-    //根据用户名得到用户数量
-    User.getUserNumByName = function getUserNumByName(account, callback) {
-
-        var getUserNumByName_Sql = "SELECT COUNT(1) AS num FROM h_user WHERE account = ?";
-
-        connection.query(getUserNumByName_Sql, [account], function (err, result) {
-            if (err) {
-                console.log("getUserNumByName Error: " + err.message);
-                return;
-            }
-
-            //connection.release();
-
-            console.log("invoked[getUserNumByName]");
-            callback(err,result);
-        });
-    };
-
-    //根据用户名得到用户信息
-    User.getUserByUserName = function getUserNumByName(account, callback) {
-
-        var getUserByUserName_Sql = "SELECT * FROM h_user WHERE account = ?";
-
-        connection.query(getUserByUserName_Sql, [account], function (err, result) {
-            if (err) {
-                console.log("getUserByUserName Error: " + err.message);
-                return;
-            }
-
-            //connection.release();
-
-            console.log("invoked[getUserByUserName]");
-            callback(err,result);
-        });
-    };
-
-    connection.release();
-});
+//读取用户信息
+User.get = function(name, callback) {
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);//错误，返回 err 信息
+    }
+    //读取 users 集合
+    db.collection('users', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);//错误，返回 err 信息
+      }
+      //查找用户名（name键）值为 name 一个文档
+      collection.findOne({
+        name: name
+      }, function (err, user) {
+        mongodb.close();
+        if (err) {
+          return callback(err);//失败！返回 err
+        }
+        callback(null, user);//成功！返回查询的用户信息
+      });
+    });
+  });
+};
